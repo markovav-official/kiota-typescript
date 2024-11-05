fdb549322005d39418652ffd2b3c350a5a08adcd
Andrei Markov <me@markovav.ru>
2024-11-05 21:17:14 +0300

    fix: using parseGuidString as validator function

--- packages/abstractions/src/utils/guidUtils.ts
+++ packages/abstractions/src/utils/guidUtils.ts
@@ -6,6 +6,15 @@
  */
 export type Guid = string
 
+const guidValidator = new RegExp("^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$", "i");
+
+export function parseGuidString(source?: string): Guid | undefined {
+  if (source && guidValidator.test(source)) {
+    return source;
+  }
+  return undefined;
+}
+
 export function createGuid(): Guid {
   return ([gen(2), gen(1), gen(1), gen(1), gen(3)].join("-"));
 }
